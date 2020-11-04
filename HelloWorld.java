d3a1d5f3e7201dd2b30de5105359b561588bc51a
Lazar Milosevic <lmilosev@smail.uni-koeln.de>
2020-11-04 11:52:46 +0100

    Nachricht hinzugefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
         public static void main( String[] args) {
           System.out.println( "Nathalie Spellerberg" ); 
           System.out.println( "3 Jahre" ); 
+          System.out.println( "Hallo ich bin Lazar" ); 
         }
         
     }
